6ac50ba589214e006116ba6df75379da956cd63d
Mirai3103 <huuhoag1412@gmail.com>
2023-11-11 01:32:29 +0700

    create fake data

--- crawler/order.js
+++ crawler/order.js
@@ -7,61 +7,6 @@ import { randomUUID } from "crypto";
 config();
 const prisma = new PrismaClient();
 
-// model payment {
-//   payment_id         String    @id @db.VarChar(36)
-//   created_by         String?   @db.VarChar(255)
-//   created_date       DateTime? @db.DateTime(6)
-//   last_modified_by   String?   @db.VarChar(255)
-//   last_modified_date DateTime? @db.DateTime(6)
-//   amount             Float     @db.Float
-//   payment_details    String    @db.VarChar(255)
-//   payment_response   String?   @db.VarChar(255)
-//   status             Int       @db.TinyInt
-//   order_order_id     String?   @db.VarChar(36)
-//   order              order?    @relation(fields: [order_order_id], references: [order_id], onDelete: Restrict, onUpdate: Restrict, map: "FKhobhqk8v7ohgfr56r725x80y5")
-
-//   @@index([order_order_id], map: "FKhobhqk8v7ohgfr56r725x80y5")
-// }
-
-// model order_item {
-//   order_id          String         @db.VarChar(255)
-//   product_option_id Int
-//   price             Float
-//   quantity          Int
-//   order             order          @relation(fields: [order_id], references: [order_id], onUpdate: Restrict, map: "FKryn1sdluxcjfeu891k75myheu")
-//   product_option    product_option @relation(fields: [product_option_id], references: [product_option_id], onUpdate: Restrict, map: "FKsoimewh387var4rtx7v1wa2wv")
-
-//   @@id([order_id, product_option_id])
-//   @@index([product_option_id], map: "FKsoimewh387var4rtx7v1wa2wv")
-// }
-
-// model order {
-//   order_id               String       @id @db.VarChar(36)
-//   created_by             String?      @db.VarChar(255)
-//   created_date           DateTime?    @db.DateTime(6)
-//   last_modified_by       String?      @db.VarChar(255)
-//   last_modified_date     DateTime?    @db.DateTime(6)
-//   address                String?      @db.VarChar(500)
-//   cancel_reason          String?      @db.VarChar(500)
-//   customer_name          String?      @db.VarChar(100)
-//   delivery_fee           Float?
-//   note                   String?      @db.VarChar(500)
-//   payment_method         Int          @db.TinyInt
-//   phone_number           String?      @db.VarChar(20)
-//   status                 Int?         @db.TinyInt
-//   total_amount           Int?
-//   promotion_promotion_id Int?
-//   user_user_id           String?      @db.VarChar(255)
-//   email                  String?      @db.VarChar(100)
-//   promotion              promotion?   @relation(fields: [promotion_promotion_id], references: [promotion_id], onDelete: Restrict, onUpdate: Restrict, map: "FKkgpbw4fb1s5r7v0tlathrxp7s")
-//   user                   user?        @relation(fields: [user_user_id], references: [user_id], onDelete: Restrict, onUpdate: Restrict, map: "FKktqkl7kou3pykq8fnm4rr9jmy")
-//   order_item             order_item[]
-//   payment                payment[]
-
-//   @@index([promotion_promotion_id], map: "FKkgpbw4fb1s5r7v0tlathrxp7s")
-//   @@index([user_user_id], map: "FKktqkl7kou3pykq8fnm4rr9jmy")
-//   @@map("_order")
-// }
 (async () => {
   const listUser = await prisma.user.findMany({
     where: {
@@ -151,30 +96,6 @@ function randomVietNameseMoney({ min = 0, max = 100000000 }) {
   const newAmount = Number(newAmountString + "000");
   return newAmount;
 }
-
-// public enum PaymentStatus {
-//     PENDING, => 0
-//     PAID, => 1
-//     CANCELLED, => 2
-//     REFUNDED, => 3
-//     FAILED, => 4
-// }
-
-// model payment {
-//   payment_id         String    @id @db.VarChar(36)
-//   created_by         String?   @db.VarChar(255)
-//   created_date       DateTime? @db.DateTime(6)
-//   last_modified_by   String?   @db.VarChar(255)
-//   last_modified_date DateTime? @db.DateTime(6)
-//   amount             Float     @db.Float
-//   payment_details    String    @db.VarChar(255)
-//   payment_response   String?   @db.VarChar(255)
-//   status             Int       @db.TinyInt
-//   order_order_id     String?   @db.VarChar(36)
-//   order              order?    @relation(fields: [order_order_id], references: [order_id], onDelete: Restrict, onUpdate: Restrict, map: "FKhobhqk8v7ohgfr56r725x80y5")
-
-//   @@index([order_order_id], map: "FKhobhqk8v7ohgfr56r725x80y5")
-// }
 
 async function createFakePayment() {
   const listOrders = await prisma.order.findMany({});
