033852534cf5d108a7500e0b8f116562deb4bd93
Mirai3103 <huuhoag1412@gmail.com>
2023-12-10 17:14:45 +0700

    su loi tim kiem category id

--- src/main/java/com/shop/clothing/product/query/advanceSearchProduct/AdvanceSearchAllProductsQueryHandler.java
+++ src/main/java/com/shop/clothing/product/query/advanceSearchProduct/AdvanceSearchAllProductsQueryHandler.java
@@ -1,5 +1,6 @@
 package com.shop.clothing.product.query.advanceSearchProduct;
 
+import com.shop.clothing.category.CategoryRepository;
 import com.shop.clothing.common.Cqrs.HandleResponse;
 import com.shop.clothing.common.Cqrs.IRequestHandler;
 import com.shop.clothing.common.dto.Paginated;
@@ -21,12 +22,21 @@ import java.util.Arrays;
 public class AdvanceSearchAllProductsQueryHandler implements IRequestHandler<AdvanceSearchAllProductsQuery, Paginated<ProductBriefDto>> {
     private final ModelMapper modelMapper;
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
 
     @Override
     @Transactional(readOnly = true)
     public HandleResponse<Paginated<ProductBriefDto>> handle(AdvanceSearchAllProductsQuery query) throws Exception {
         if (query.getSizes() != null)
             query.setSizes(Arrays.stream(query.getSizes()).map(String::toUpperCase).toArray(String[]::new));
+        var categoryIds = categoryRepository.findAllByParentCategoryIdIn(query.getCategoryIds());
+        if (query.getCategoryIds() != null) {
+            categoryIds.addAll(Arrays.stream(query.getCategoryIds()).boxed().toList());
+        }
+        query.setCategoryIds(categoryIds.stream().mapToInt(Integer::intValue).toArray());
+        if (query.getCategoryIds().length == 0) {
+            query.setCategoryIds(null);
+        }
         var productPage = productRepository.searchAllProducts(query.getKeyword(), query.getCategoryIds(), query.getForGenders(), query.getMinPrice(), query.getMaxPrice(), query.getColorIds(), query.getSizes(), query.getPageable("product.createdDate"));
         Paginated<ProductBriefDto> paginated = Paginated.of(productPage.map(product -> modelMapper.map(product, ProductBriefDto.class)));
         return HandleResponse.ok(paginated);
